1db82c73aeb512504139b9a3c89d470b659d2ccb
mam-mih-val <mam.mih.val@gmail.com>
2021-10-08 13:46:23 +0200

    Fix

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -19,8 +19,8 @@ echo >> list.txt
 
 module use /cvmfs/vae.gsi.de/centos7/modules/linux-centos7-x86_64/cmake-3.17.3-gcc-8.1.0-3354fvg
 
-echo "loading /lustre/hades/user/mmamaev/install/root-6.20.04-centos7-cxx17/bin/thisroot.sh"
-source /lustre/hades/user/mmamaev/install/root-6.20.04-centos7-cxx17/bin/thisroot.sh
+echo "loading /lustre/hades/user/mmamaev/install/root-6.18.04-centos7-cxx17/bin/thisroot.sh"
+source /lustre/hades/user/mmamaev/install/root-6.18.04-centos7-cxx17/bin/thisroot.sh
 
 echo "executing $build_dir/acceptance list.txt $pdg_code"
 $build_dir/acceptance list.txt $pdg_code
