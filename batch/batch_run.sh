2a6469b60f8bf8990499eb218f97d34cad05ee6b
mam-mih-val <mam.mih.val@gmail.com>
2021-11-28 10:39:59 +0100

    Job submiting updated

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -22,7 +22,7 @@ module use /cvmfs/vae.gsi.de/centos7/modules/linux-centos7-x86_64/cmake-3.17.3-g
 echo "loading /lustre/hades/user/mmamaev/install/root-6.18.04-centos7-cxx17/bin/thisroot.sh"
 source /lustre/hades/user/mmamaev/install/root-6.18.04-centos7-cxx17/bin/thisroot.sh
 
-echo "executing $build_dir/acceptance list.txt $pdg_code"
-$build_dir/acceptance list.txt $pdg_code
+echo "executing $build_dir/acceptance list.txt $pdg_code /lustre/hebe/hades/user/bkardan/param/centrality_epcorr_mar19_ag123ag_2500A_glauber_gen4_2020_10_pass3.root"
+$build_dir/acceptance list.txt $pdg_code /lustre/hebe/hades/user/bkardan/param/centrality_epcorr_mar19_ag123ag_2500A_glauber_gen4_2020_10_pass3.root
 
 echo JOB FINISHED!
